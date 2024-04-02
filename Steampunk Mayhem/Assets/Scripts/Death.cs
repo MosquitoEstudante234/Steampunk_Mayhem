49c7454e54ad0ef25b7c04233cf006aa47db8b28
MosquitoEstudante234 <mosquitoestudante34234@gmail.com>
2024-04-02 14:38:18 -0300

    mais sprites / inimigos implementados

---
+++ Steampunk Mayhem/Assets/Scripts/Death.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Death : MonoBehaviour
+{
+    
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Enemy"))
+        {
+            SceneManager.LoadScene("cena0");
+        }
+    }
+}
